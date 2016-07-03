de2345446f780b7250e1b1c29dcd2083454a6603
Henrik Brautaset Aronsen <henrik@synth.no>
2016-07-03 19:00:00 +0200

    Fix file.js indenting

--- src/components/file.js
+++ src/components/file.js
@@ -1,43 +1,43 @@
-var clone = function(someObj) {
-    return JSON.parse(JSON.stringify(someObj));
+var clone = function (someObj) {
+  return JSON.parse(JSON.stringify(someObj));
 };
 
-var saveJson = function(content, filename) {
-    let blob = new Blob([content], {
-        type: 'application/json'
-    });
-    if (typeof window.navigator.msSaveBlob !== 'undefined') {
-        window.navigator.msSaveBlob(blob, filename); //IE workaround
-    } else {
-        let elem = document.createElement('a');
-        elem.href = window.URL.createObjectURL(blob);
-        elem.setAttribute('download', filename);
-        elem.click();
-    }
+var saveJson = function (content, filename) {
+  let blob = new Blob([content], {
+    type: 'application/json'
+  });
+  if (typeof window.navigator.msSaveBlob !== 'undefined') {
+    window.navigator.msSaveBlob(blob, filename); //IE workaround
+  } else {
+    let elem = document.createElement('a');
+    elem.href = window.URL.createObjectURL(blob);
+    elem.setAttribute('download', filename);
+    elem.click();
+  }
 };
 
 export function saveState(state) {
-    let saveState = clone(state);
-    Object.keys(saveState).forEach((key) =>
-        key.match(/^transient/) && delete saveState[key]
-    );
-    saveJson(JSON.stringify(saveState), "stickmap.json");
+  let saveState = clone(state);
+  Object.keys(saveState).forEach((key) =>
+      key.match(/^transient/) && delete saveState[key]
+  );
+  saveJson(JSON.stringify(saveState), "stickmap.json");
 }
 
 export function loadState(setNewState, event) {
-    let file = event.target.files[0];
-    let reader = new FileReader();
-    reader.onload = (fileEvent) => {
-        let errormsg = "Error: " + file.name + " is not a stickmap state file";
-        try {
-            var newState = JSON.parse(fileEvent.target.result);
-            setNewState(newState);
-            return;
-        } catch (ex) {
-            errormsg += ": " + ex.toString();
-        }
+  let file = event.target.files[0];
+  let reader = new FileReader();
+  reader.onload = (fileEvent) => {
+    let errormsg = "Error: " + file.name + " is not a stickmap state file";
+    try {
+      var newState = JSON.parse(fileEvent.target.result);
+      setNewState(newState);
+      return;
+    } catch (ex) {
+      errormsg += ": " + ex.toString();
+    }
 
-        alert(errormsg);
-    };
-    reader.readAsText(file);
+    alert(errormsg);
+  };
+  reader.readAsText(file);
 }
